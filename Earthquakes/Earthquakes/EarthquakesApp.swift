615be8527ee8782acfe3ea557344e3e57cdd24da
Oriol Angelet <uriaeri@gmail.com>
2025-09-26 18:44:02 +0200

    Initial commit, including the Quake model & QuakeError error enum

---
+++ Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Uri on 26/9/25.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
