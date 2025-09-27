68f998655439614739d7b99180b86e7be968da2f
Oriol Angelet <uriaeri@gmail.com>
2025-09-27 16:30:32 +0200

    Added GeoJSON model

---
+++ Earthquakes/Earthquakes/Models/GeoJSON.swift
@@ -0,0 +1,42 @@
+//
+//  GeoJSON.swift
+//  Earthquakes
+//
+//  Created by Uri on 27/9/25.
+//
+
+import Foundation
+
+struct GeoJSON: Decodable {
+    
+    // Tells the decoder which keys of the api we want to decode
+    private enum RootCodingKeys: String, CodingKey {
+        case features
+    }
+    
+    private enum FeatureCodingKeys: String, CodingKey {
+        case properties
+    }
+    
+    // private(set) = only read outside the class
+    private(set) var quakes: [Quake] = []
+    
+    init(from decoder: Decoder) throws {
+        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
+        var featuresContainer = try rootContainer.nestedUnkeyedContainer(forKey: .features)
+        
+        while !featuresContainer.isAtEnd {
+            let propertiesContainer = try decoder.container(keyedBy: FeatureCodingKeys.self)
+            
+            if let properties = try? propertiesContainer.decode(Quake.self, forKey: .properties) {
+                quakes.append(properties)
+            }
+        }
+    }
+}
+
+/*
+ Json -> Features > Properties
+ */
+
+// featuresContainer extracts quakes, one at a time. The decoder accesses the elements chronologically.
