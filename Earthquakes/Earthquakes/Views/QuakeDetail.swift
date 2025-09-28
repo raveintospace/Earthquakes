89252c7bf08626de7375f281495b39c7d2c61fed
Oriol Angelet <uriaeri@gmail.com>
2025-09-28 13:52:46 +0200

    App finished - Integrated QuakeDetailMap view to QuakeDetail view

--- Earthquakes/Earthquakes/Views/QuakeDetail.swift
+++ Earthquakes/Earthquakes/Views/QuakeDetail.swift
@@ -17,6 +17,11 @@ struct QuakeDetail: View {
     
     var body: some View {
         VStack {
+            if let location = self.location {
+                QuakeDetailMap(location: location, tintColor: quake.color)
+                    .ignoresSafeArea(.container)
+            }
+            
             QuakeMagnitude(quake: quake)
             
             Text(quake.place)
