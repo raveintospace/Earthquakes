6ed1b842634e88e44eb27f4bf48404a6a4eb2402
Oriol Angelet <uriaeri@gmail.com>
2025-09-28 12:58:10 +0200

    Updated QuakeClient to be an Actor instead of Struct // Added method to fetch locations asynchronously

--- Earthquakes/Earthquakes/Models/QuakeClient.swift
+++ Earthquakes/Earthquakes/Models/QuakeClient.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct QuakeClient {
+// Actor protects the cache from simultaneous access from multiple threads (when fetching locations)
+actor QuakeClient {
     
     private let downloader: any HTTPDataDownloader
     private let feedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")!
@@ -19,7 +20,7 @@ struct QuakeClient {
         return aDecoder
     }()
     
-    // Fetches the earthquakes
+    /// Fetches the earthquakes
     var quakes: [Quake] {
         get async throws {
             let data = try await downloader.httpData(from: feedURL)
@@ -30,5 +31,31 @@ struct QuakeClient {
     
     init(downloader: any HTTPDataDownloader = URLSession.shared) {
         self.downloader = downloader
+    }
+    
+    /// Fetches and decodes the location
+    func quakeLocation(from url: URL) async throws -> QuakeLocation {
+        if let cached = await quakeCache[url] {
+            switch cached {
+            case .ready(let location):
+                return location
+            case .inProgress(let task):         // avoids making a second network request
+                return try await task.value
+            }
+        }
+        let task = Task<QuakeLocation, Error> {
+            let data = try await downloader.httpData(from: url)
+            let location = try decoder.decode(QuakeLocation.self, from: data)
+            return location
+        }
+        quakeCache[url] = .inProgress(task)
+        do {
+            let location = try await task.value
+            quakeCache[url] = .ready(location)
+            return location
+        } catch {
+            quakeCache[url] = nil
+            throw error
+        }
     }
 }
